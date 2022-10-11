4d8c9c7c5ccddfd07d3cc92f0a82e977c27496b9
무영 김 <limm123@daum.net>
2022-10-11 21:09:29 +0900

    클래스 연습 추가

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        class Add_Sub
+        {
+            int num;
+            public Add_Sub(int num = 0)
+            {
+                this.num = num;
+            }
+            public void Add(int b)
+            {
+                int a = num;
+                num += b;
+                Console.WriteLine($"{a} + {b} = {num}");
+            }
+
+            public void Sub(int b)
+            {
+                int a = num;
+                num -= b;
+                Console.WriteLine($"{a} - {b} = {num}");
+            }
+            public void Clear()
+            {
+                num = 0;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Add_Sub A = new Add_Sub();
+            int check = 0, num;
+            while(check != 4)
+            {
+                Console.WriteLine("------------------------------------");
+                Console.WriteLine("Add(1), Sub(2), clear(3), eixt(4)");
+                Console.Write("=>");
+                check = int.Parse(Console.ReadLine());
+                switch (check)
+                {
+                    case 1: 
+                        Console.WriteLine("------------------------------------");
+                        Console.Write("Write Added num => ");
+                        num = int.Parse(Console.ReadLine());
+                        A.Add(num);
+                        break;
+                    case 2:
+                        Console.WriteLine("------------------------------------");
+                        Console.Write("Write Subed num => ");
+                        num = int.Parse(Console.ReadLine());
+                        A.Sub(num);
+                        break;
+                    case 3:
+                        Console.WriteLine("------------------------------------");
+                        A.Clear();
+                        Console.WriteLine("Clear Complete");
+                        break;
+                    case 4:
+                        Console.WriteLine("exit program");
+                        break;
+                }
+            }
+        }
+    }
+}
