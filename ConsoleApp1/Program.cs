f5ad894983f1f34f9576c5fa25e4f1cb3ece4e66
무영 김 <limm123@daum.net>
2022-12-17 03:48:54 +0900

    test

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,7 +15,7 @@ namespace ConsoleApp1
             string buff1 = Rdouble.ReadLine();
             String[] buff2 = buff1.Split(' ');
             double C = double.Parse(buff2[0]);
-            int A = int.Parse(buff2[1]), B = int.Parse(buff2[2]);
+            int A = int.Parse(buff2[1]), B = int.Parse(buff2[2]); 
             double result = 0;
             Wdouble.Write("("); 
             for(int i = A; i<=B; i++)
